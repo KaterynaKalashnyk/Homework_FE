283c97d9a9afb82b6ed3e9024c28d66d2b4f91ac
KaterynaKalashnyk <122403309+KaterynaKalashnyk@users.noreply.github.com>
2023-02-17 16:40:33 +0100

    Add files via upload

---
+++ HW_3/9/script/script.js
@@ -0,0 +1,8 @@
+// Напишите функцию power, 
+// которая в качестве аргумента n принимает число и возвращает квадрат этого числа.
+
+function power(n){
+    result = n**2
+    return result
+}
+console.log(power(8))
