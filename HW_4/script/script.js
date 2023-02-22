189438327350c6b02fe77a89aa575d867182fbe4
KaterynaKalashnyk <122403309+KaterynaKalashnyk@users.noreply.github.com>
2023-02-22 22:07:18 +0100

    Add files via upload

---
+++ HW_4/script/script.js
@@ -0,0 +1,21 @@
+const bth1 = document.querySelector('.bth1');
+const bth2 = document.querySelector('.bth2');
+const bth3 = document.querySelector('.bth3');
+const bth4 = document.querySelector('.bth4');
+const bth5 = document.querySelector('.bth5');
+
+bth1.addEventListener('click', function(){
+	alert("#227093")
+})
+bth2.addEventListener('click', function(){
+	alert("#218c74")
+})
+bth3.addEventListener('click', function(){
+	alert("#fff200")
+})
+bth4.addEventListener('click', function(){
+	alert("#ffb8b8")
+})
+bth5.addEventListener('click', function(){
+	alert("#ff3838")
+})
